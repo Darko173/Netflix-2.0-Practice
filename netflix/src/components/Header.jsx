e03da8cf93380ea440af38d1e002cd4b27e6f23e
SayaN <sayansarkar173@gmail.com>
2023-02-20 21:23:22 +0530

    Header
    debugging

--- netflix/src/components/Header.jsx
+++ netflix/src/components/Header.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from "../assets/logo.png"
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default   function Header(props) {
-
-const navigate= useNavigate();
   return (
     <Container className='flex a-center j-between'>
         <div className="logo">
             <img src={logo} alt="logo" />
         </div>
-        <button onClick={()=> navigate(props.login ? "/login": "/signup")}>{props.login ? "Log In": "Sign In"}</button>
+        <button onClick={()=> Navigate(props.login ? "/login": "/signup")}>{props.login ? "Log In": "Sign In"}</button>
 
     </Container>
     
